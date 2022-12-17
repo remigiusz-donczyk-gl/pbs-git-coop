e71219afc076ccfc8970fec15894dd0654ab1d53
szymon <szymonmaruda5@gmail.com>
2022-12-17 10:16:49 +0100

    Program służący do wypisywania tablicy liczbowej od tyłu

---
+++ zad6_1.c
@@ -0,0 +1,15 @@
+#include <stadio.h>
+int tab1[5];
+int main(){
+ int i=0;
+ printf("podaj 5 liczb\n");
+ while (i<5){
+  printf("podaj liczbe\n");
+  scanf("%d",&tab1[i]);
+  i++;
+ }
+ while(i>0){
+  i--;
+  printf("%d",tab1[i];
+ }
+}
